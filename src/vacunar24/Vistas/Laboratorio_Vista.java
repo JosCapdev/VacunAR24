259e6b8021a44d88fff4f0518e7a0f7651bf39ae
Jose <141686465+JosCapdev@users.noreply.github.com>
2024-01-31 15:48:06 -0300

    Update Laboratorio_Vista.java

--- src/vacunar24/Vistas/Laboratorio_Vista.java
+++ src/vacunar24/Vistas/Laboratorio_Vista.java
@@ -5,17 +5,37 @@
  */
 package vacunar24.Vistas;
 
+import java.awt.Frame;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import vacunar24.Dao.LaboratorioData;
+import vacunar24.Entidades.Laboratorio;
+
 /**
  *
  * @author Jose
  */
 public class Laboratorio_Vista extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form Laboratorio_Vista
-     */
+    private Laboratorio lab;
+    private LaboratorioData labD;
+    private ArrayList<Laboratorio> listaLab;
+    private DefaultTableModel modelo = new DefaultTableModel() {
+        public boolean isCellEditable(int row, int colum) {
+            return false;
+        }
+    };
+    private Frame f = JOptionPane.getFrameForComponent(this);
+    private AgregarLaboratorio al = new AgregarLaboratorio(f, true);
+
     public Laboratorio_Vista() {
+        lab = new Laboratorio();
+        labD = new LaboratorioData();
+        listaLab = new ArrayList();
         initComponents();
+        armarCabecera();
+        llenarTabla();
     }
 
     /**
@@ -29,45 +49,25 @@ public class Laboratorio_Vista extends javax.swing.JInternalFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-
-        setPreferredSize(new java.awt.Dimension(1300, 550));
+        jTLab = new javax.swing.JTable();
+        jBAgregar = new javax.swing.JButton();
+        jBMod = new javax.swing.JButton();
+        jBElim = new javax.swing.JButton();
+        jTBuscador = new javax.swing.JTextField();
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                jPanel1MouseMoved(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setText("CONTROL Y REGISTRO DE LABORATORIOS:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 6, 308, -1));
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, 142, -1));
-        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, 140, -1));
-        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 210, 142, -1));
-        jPanel1.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 270, 142, -1));
+        jLabel1.setText("REGISTRO Y CONTROL DE LABORATORIOS:");
 
-        jButton1.setFont(new java.awt.Font("Arial", 2, 12)); // NOI18N
-        jButton1.setText("Agregar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 350, 89, -1));
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTLab.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -78,38 +78,74 @@ public class Laboratorio_Vista extends javax.swing.JInternalFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTLab);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 60, 1110, 270));
+        jBAgregar.setText("Agregar laboratorio");
+        jBAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBAgregarActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("Limpiar Campos");
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 350, -1, -1));
+        jBMod.setText("Modificar");
+        jBMod.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBModActionPerformed(evt);
+            }
+        });
 
-        jButton3.setText("Eliminar");
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1150, 350, -1, -1));
+        jBElim.setText("Eliminar");
+        jBElim.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBElimActionPerformed(evt);
+            }
+        });
 
-        jButton4.setText("Modificar");
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 350, -1, -1));
+        jTBuscador.setText("Buscar...");
+        jTBuscador.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTBuscadorKeyReleased(evt);
+            }
+        });
 
-        jLabel2.setFont(new java.awt.Font("Dialog", 2, 12)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel2.setText("Dominio Comercial:");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 250, -1, -1));
-
-        jLabel3.setFont(new java.awt.Font("Dialog", 2, 12)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel3.setText("Cuit:");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, -1, -1));
-
-        jLabel4.setFont(new java.awt.Font("Dialog", 2, 12)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel4.setText("Nombre del Laboratorio:");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, -1, -1));
-
-        jLabel5.setFont(new java.awt.Font("Dialog", 2, 12)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel5.setText("País:");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 190, -1, -1));
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1286, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTBuscador, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(88, 88, 88)
+                .addComponent(jBAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jBMod)
+                .addGap(45, 45, 45)
+                .addComponent(jBElim)
+                .addGap(15, 15, 15))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTBuscador, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBAgregar)
+                    .addComponent(jBMod)
+                    .addComponent(jBElim))
+                .addGap(16, 16, 16))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -125,27 +161,85 @@ public class Laboratorio_Vista extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jBAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBAgregarActionPerformed
+        al.show();
+    }//GEN-LAST:event_jBAgregarActionPerformed
 
+    private void jBModActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBModActionPerformed
+        if (jTLab.getSelectedRow() >= 0) {
+            lab = labD.buscarLaboratorioId((int) jTLab.getValueAt(jTLab.getSelectedRow(), 0));
+            al.getjTCuit().setText(lab.getCuit()+"");
+            al.getjTNomL().setText(lab.getNomLaboratorio());
+            al.getjTPais().setText(lab.getPais());
+            al.getjTDomC().setText(lab.getDomComercial());
+            al.setIdMod(lab.getIdLaboratorio());
+            al.setMod(true);
+            al.show();
+        } else {
+            JOptionPane.showMessageDialog(this, "Seleccione un laboratorio en la tabla...");
+        }
+    }//GEN-LAST:event_jBModActionPerformed
+
+    private void jBElimActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBElimActionPerformed
+        if (jTLab.getSelectedRow() >= 0) {
+            labD.eliminarLaboratorio((int) jTLab.getValueAt(jTLab.getSelectedRow(), 0));
+            actT();
+        } else {
+            JOptionPane.showMessageDialog(this, "Seleccione un laboratorio en la tabla...");
+        }
+    }//GEN-LAST:event_jBElimActionPerformed
+
+    private void jPanel1MouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseMoved
+        if (al.isAct()) {
+            actT();
+        }
+    }//GEN-LAST:event_jPanel1MouseMoved
+
+    private void jTBuscadorKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTBuscadorKeyReleased
+        modelo.setNumRows(0);
+        for (Laboratorio l : listaLab) {
+            if (l.getNomLaboratorio().toLowerCase().startsWith(jTBuscador.getText().toLowerCase()) || l.getCuit() == Integer.parseInt(jTBuscador.getText())
+                    || l.getIdLaboratorio() == Integer.parseInt(jTBuscador.getText()) || l.getDomComercial().toLowerCase().startsWith(jTBuscador.getText().toLowerCase())) {
+                modelo.addRow(new Object[]{l.getIdLaboratorio(),l.getCuit(),l.getNomLaboratorio(),l.getPais(),l.getDomComercial(),l.isEstado()});
+            }
+        }
+    }//GEN-LAST:event_jTBuscadorKeyReleased
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jBAgregar;
+    private javax.swing.JButton jBElim;
+    private javax.swing.JButton jBMod;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTBuscador;
+    private javax.swing.JTable jTLab;
     // End of variables declaration//GEN-END:variables
+    private void armarCabecera() {
+        ArrayList<Object> filaCabecera = new ArrayList<>();
+        filaCabecera.add("Codigo");
+        filaCabecera.add("Cuit");
+        filaCabecera.add("Nombre Laboratorio");
+        filaCabecera.add("País");
+        filaCabecera.add("Dominio Comercial");
+        filaCabecera.add("Estado");
+        for (Object it : filaCabecera) {
+            modelo.addColumn(it);
+        }
+        jTLab.setModel(modelo);
+    }
+
+    private void llenarTabla() {
+        listaLab = labD.listarLaboratorios();
+        listaLab.stream().forEach(l -> {
+            modelo.addRow(new Object[]{l.getIdLaboratorio(),l.getCuit(),l.getNomLaboratorio(),
+                l.getPais(),l.getDomComercial(),l.isEstado()});
+        });
+    }
+
+    private void actT() {
+        modelo.setNumRows(0);
+        llenarTabla();
+        al.setAct(false);
+    }
 }
