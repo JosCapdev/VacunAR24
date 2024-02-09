443d2e0cf06ffeb7755e7f91b9f9d441f174740d
Jose <141686465+JosCapdev@users.noreply.github.com>
2024-02-08 22:29:59 -0300

    Update Menu.java

--- src/vacunar24/Vistas/Menu.java
+++ src/vacunar24/Vistas/Menu.java
@@ -5,6 +5,8 @@
  */
 package vacunar24.Vistas;
 
+import java.awt.Color;
+
 /**
  *
  * @author Jose
@@ -17,7 +19,7 @@ public class Menu extends javax.swing.JFrame {
     public Menu() {
         initComponents();
         this.setLocationRelativeTo(null);
-       
+
     }
 
     /**
@@ -32,17 +34,17 @@ public class Menu extends javax.swing.JFrame {
         backgroundPanel = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        jPanelTurno = new javax.swing.JPanel();
         jLTurno = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
+        jPanelCiudadano = new javax.swing.JPanel();
         jLCiudadano = new javax.swing.JLabel();
-        jPanel4 = new javax.swing.JPanel();
+        jPanelProfesional = new javax.swing.JPanel();
         jLProfesional = new javax.swing.JLabel();
-        jPanel5 = new javax.swing.JPanel();
+        jPanelVacuna = new javax.swing.JPanel();
         jLVacuna = new javax.swing.JLabel();
         jPFondoCel = new javax.swing.JPanel();
         Escritorio = new javax.swing.JPanel();
-        jPanel6 = new javax.swing.JPanel();
+        jPanelLaboratorio = new javax.swing.JPanel();
         jLLaboratorio = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -79,8 +81,8 @@ public class Menu extends javax.swing.JFrame {
 
         backgroundPanel.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 70));
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(0, 153, 255)));
+        jPanelTurno.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelTurno.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(0, 153, 255)));
 
         jLTurno.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLTurno.setForeground(new java.awt.Color(51, 153, 255));
@@ -89,29 +91,35 @@ public class Menu extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jLTurnoMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jLTurnoMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jLTurnoMouseExited(evt);
+            }
         });
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanelTurnoLayout = new javax.swing.GroupLayout(jPanelTurno);
+        jPanelTurno.setLayout(jPanelTurnoLayout);
+        jPanelTurnoLayout.setHorizontalGroup(
+            jPanelTurnoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelTurnoLayout.createSequentialGroup()
                 .addContainerGap(43, Short.MAX_VALUE)
                 .addComponent(jLTurno)
                 .addGap(16, 16, 16))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        jPanelTurnoLayout.setVerticalGroup(
+            jPanelTurnoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelTurnoLayout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(jLTurno)
                 .addContainerGap(28, Short.MAX_VALUE))
         );
 
-        backgroundPanel.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 0, 110, -1));
+        backgroundPanel.add(jPanelTurno, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 0, 110, -1));
 
-        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel3.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
+        jPanelCiudadano.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelCiudadano.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
 
         jLCiudadano.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLCiudadano.setForeground(new java.awt.Color(51, 153, 255));
@@ -120,29 +128,35 @@ public class Menu extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jLCiudadanoMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jLCiudadanoMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jLCiudadanoMouseExited(evt);
+            }
         });
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanelCiudadanoLayout = new javax.swing.GroupLayout(jPanelCiudadano);
+        jPanelCiudadano.setLayout(jPanelCiudadanoLayout);
+        jPanelCiudadanoLayout.setHorizontalGroup(
+            jPanelCiudadanoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelCiudadanoLayout.createSequentialGroup()
                 .addContainerGap(39, Short.MAX_VALUE)
                 .addComponent(jLCiudadano)
                 .addContainerGap())
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        jPanelCiudadanoLayout.setVerticalGroup(
+            jPanelCiudadanoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelCiudadanoLayout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(jLCiudadano)
                 .addContainerGap(28, Short.MAX_VALUE))
         );
 
-        backgroundPanel.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 0, 130, 70));
+        backgroundPanel.add(jPanelCiudadano, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 0, 130, 70));
 
-        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel4.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
+        jPanelProfesional.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelProfesional.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
 
         jLProfesional.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLProfesional.setForeground(new java.awt.Color(51, 153, 255));
@@ -151,29 +165,35 @@ public class Menu extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jLProfesionalMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jLProfesionalMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jLProfesionalMouseExited(evt);
+            }
         });
 
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel4Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanelProfesionalLayout = new javax.swing.GroupLayout(jPanelProfesional);
+        jPanelProfesional.setLayout(jPanelProfesionalLayout);
+        jPanelProfesionalLayout.setHorizontalGroup(
+            jPanelProfesionalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelProfesionalLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLProfesional)
                 .addContainerGap(22, Short.MAX_VALUE))
         );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel4Layout.createSequentialGroup()
+        jPanelProfesionalLayout.setVerticalGroup(
+            jPanelProfesionalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelProfesionalLayout.createSequentialGroup()
                 .addGap(22, 22, 22)
                 .addComponent(jLProfesional)
                 .addContainerGap(29, Short.MAX_VALUE))
         );
 
-        backgroundPanel.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 0, 110, 70));
+        backgroundPanel.add(jPanelProfesional, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 0, 110, 70));
 
-        jPanel5.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel5.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
+        jPanelVacuna.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelVacuna.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
 
         jLVacuna.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLVacuna.setForeground(new java.awt.Color(51, 153, 255));
@@ -182,26 +202,32 @@ public class Menu extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jLVacunaMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jLVacunaMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jLVacunaMouseExited(evt);
+            }
         });
 
-        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
-        jPanel5.setLayout(jPanel5Layout);
-        jPanel5Layout.setHorizontalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanelVacunaLayout = new javax.swing.GroupLayout(jPanelVacuna);
+        jPanelVacuna.setLayout(jPanelVacunaLayout);
+        jPanelVacunaLayout.setHorizontalGroup(
+            jPanelVacunaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelVacunaLayout.createSequentialGroup()
                 .addGap(16, 16, 16)
                 .addComponent(jLVacuna)
                 .addContainerGap(51, Short.MAX_VALUE))
         );
-        jPanel5Layout.setVerticalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
+        jPanelVacunaLayout.setVerticalGroup(
+            jPanelVacunaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelVacunaLayout.createSequentialGroup()
                 .addGap(22, 22, 22)
                 .addComponent(jLVacuna)
                 .addContainerGap(29, Short.MAX_VALUE))
         );
 
-        backgroundPanel.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 0, 120, 70));
+        backgroundPanel.add(jPanelVacuna, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 0, 120, 70));
 
         jPFondoCel.setBackground(new java.awt.Color(51, 153, 255));
         jPFondoCel.setPreferredSize(new java.awt.Dimension(667, 320));
@@ -238,8 +264,8 @@ public class Menu extends javax.swing.JFrame {
 
         backgroundPanel.add(jPFondoCel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 1310, 700));
 
-        jPanel6.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel6.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
+        jPanelLaboratorio.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelLaboratorio.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(51, 153, 255)));
 
         jLLaboratorio.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLLaboratorio.setForeground(new java.awt.Color(51, 153, 255));
@@ -248,26 +274,32 @@ public class Menu extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jLLaboratorioMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jLLaboratorioMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jLLaboratorioMouseExited(evt);
+            }
         });
 
-        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
-        jPanel6.setLayout(jPanel6Layout);
-        jPanel6Layout.setHorizontalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel6Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanelLaboratorioLayout = new javax.swing.GroupLayout(jPanelLaboratorio);
+        jPanelLaboratorio.setLayout(jPanelLaboratorioLayout);
+        jPanelLaboratorioLayout.setHorizontalGroup(
+            jPanelLaboratorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelLaboratorioLayout.createSequentialGroup()
                 .addGap(16, 16, 16)
                 .addComponent(jLLaboratorio)
                 .addContainerGap(23, Short.MAX_VALUE))
         );
-        jPanel6Layout.setVerticalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel6Layout.createSequentialGroup()
+        jPanelLaboratorioLayout.setVerticalGroup(
+            jPanelLaboratorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelLaboratorioLayout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addComponent(jLLaboratorio)
                 .addContainerGap(27, Short.MAX_VALUE))
         );
 
-        backgroundPanel.add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1170, 0, 130, 70));
+        backgroundPanel.add(jPanelLaboratorio, new org.netbeans.lib.awtextra.AbsoluteConstraints(1170, 0, 130, 70));
 
         getContentPane().add(backgroundPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 1300, 790));
 
@@ -315,9 +347,57 @@ public class Menu extends javax.swing.JFrame {
         citaV.setVisible(true);
     }//GEN-LAST:event_jLTurnoMouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
+    private void jLTurnoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLTurnoMouseEntered
+        jPanelTurno.setBackground(new Color(51, 153, 255));
+        jLTurno.setForeground(Color.WHITE);
+    }//GEN-LAST:event_jLTurnoMouseEntered
+
+    private void jLTurnoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLTurnoMouseExited
+        jPanelTurno.setBackground(Color.WHITE);
+        jLTurno.setForeground(new Color(51, 153, 255));
+
+    }//GEN-LAST:event_jLTurnoMouseExited
+
+    private void jLCiudadanoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLCiudadanoMouseEntered
+        jPanelCiudadano.setBackground(new Color(51, 153, 255));
+        jLCiudadano.setForeground(Color.WHITE);
+    }//GEN-LAST:event_jLCiudadanoMouseEntered
+
+    private void jLCiudadanoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLCiudadanoMouseExited
+        jPanelCiudadano.setBackground(Color.WHITE);
+        jLCiudadano.setForeground(new Color(51, 153, 255));
+    }//GEN-LAST:event_jLCiudadanoMouseExited
+
+    private void jLProfesionalMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLProfesionalMouseEntered
+        jPanelProfesional.setBackground(new Color(51, 153, 255));
+        jLProfesional.setForeground(Color.WHITE);
+    }//GEN-LAST:event_jLProfesionalMouseEntered
+
+    private void jLProfesionalMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLProfesionalMouseExited
+        jPanelProfesional.setBackground(Color.WHITE);
+        jLProfesional.setForeground(new Color(51, 153, 255));
+    }//GEN-LAST:event_jLProfesionalMouseExited
+
+    private void jLVacunaMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLVacunaMouseEntered
+        jPanelVacuna.setBackground(new Color(51, 153, 255));
+        jLVacuna.setForeground(Color.WHITE);
+    }//GEN-LAST:event_jLVacunaMouseEntered
+
+    private void jLVacunaMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLVacunaMouseExited
+        jPanelVacuna.setBackground(Color.WHITE);
+        jLVacuna.setForeground(new Color(51, 153, 255));
+    }//GEN-LAST:event_jLVacunaMouseExited
+
+    private void jLLaboratorioMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLLaboratorioMouseEntered
+        jPanelLaboratorio.setBackground(new Color(51, 153, 255));
+        jLLaboratorio.setForeground(Color.WHITE);
+    }//GEN-LAST:event_jLLaboratorioMouseEntered
+
+    private void jLLaboratorioMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLLaboratorioMouseExited
+        jPanelLaboratorio.setBackground(Color.WHITE);
+        jLLaboratorio.setForeground(new Color(51, 153, 255));
+    }//GEN-LAST:event_jLLaboratorioMouseExited
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -361,10 +441,10 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPFondoCel;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
+    private javax.swing.JPanel jPanelCiudadano;
+    private javax.swing.JPanel jPanelLaboratorio;
+    private javax.swing.JPanel jPanelProfesional;
+    private javax.swing.JPanel jPanelTurno;
+    private javax.swing.JPanel jPanelVacuna;
     // End of variables declaration//GEN-END:variables
 }
