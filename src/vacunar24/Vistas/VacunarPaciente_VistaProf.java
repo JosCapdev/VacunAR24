e1674565b42b9f1fe85c8cea3620eaf800a52f80
Jose <141686465+JosCapdev@users.noreply.github.com>
2024-03-27 19:52:27 -0300

    Update VacunarPaciente_VistaProf.java

--- src/vacunar24/Vistas/VacunarPaciente_VistaProf.java
+++ src/vacunar24/Vistas/VacunarPaciente_VistaProf.java
@@ -156,7 +156,8 @@ public class VacunarPaciente_VistaProf extends javax.swing.JInternalFrame {
         borrarFilas();
         for (CitaVacunacion cv : listaCitaV) {
             if (cv.getPersona().getNombre().toLowerCase().startsWith(jTBuscador.getText().toLowerCase()) || cv.getPersona().getApellido().toLowerCase().startsWith(jTBuscador.getText().toLowerCase())
-                    || String.valueOf(cv.getPersona().getDni()).equals(jTBuscador.getText())){
+                    || String.valueOf(cv.getPersona().getDni()).equals(jTBuscador.getText()) || 
+                    String.valueOf(cv.getIdCitaVacunacion()).equals(jTBuscador.getText())){
                 modelo.addRow(new Object[]{cv.getIdCitaVacunacion(), cv.getPersona().getDni(), cv.getCodRefuerzo()
                         , cv.getFechaHoraCita(), cv.getCentroVacunacion(),cv.getDosis().getMarca()});            }
         }
