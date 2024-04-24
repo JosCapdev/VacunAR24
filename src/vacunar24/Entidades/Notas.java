157a185235a873f02003005e2ca774a76b259d4e
Jose <josecapdevila512@gmail.com>
2024-04-24 14:10:21 -0300

    Create Notas.java

---
+++ src/vacunar24/Entidades/Notas.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vacunar24.Entidades;
+
+/**
+ *
+ * @author Jose
+ */
+public class Notas {
+    private String not1;
+    private String not2;
+    private String not3;
+    private String not4;
+    private String not5;
+    private String not6;
+    private String not7;
+
+    public Notas() {
+    }
+    
+    public Notas(String not1, String not2, String not3, String not4, String not5, String not6, String not7) {
+        this.not1 = not1;
+        this.not2 = not2;
+        this.not3 = not3;
+        this.not4 = not4;
+        this.not5 = not5;
+        this.not6 = not6;
+        this.not7 = not7;
+    }
+
+    public String getNot1() {
+        return not1;
+    }
+
+    public void setNot1(String not1) {
+        this.not1 = not1;
+    }
+
+    public String getNot2() {
+        return not2;
+    }
+
+    public void setNot2(String not2) {
+        this.not2 = not2;
+    }
+
+    public String getNot3() {
+        return not3;
+    }
+
+    public void setNot3(String not3) {
+        this.not3 = not3;
+    }
+
+    public String getNot4() {
+        return not4;
+    }
+
+    public void setNot4(String not4) {
+        this.not4 = not4;
+    }
+
+    public String getNot5() {
+        return not5;
+    }
+
+    public void setNot5(String not5) {
+        this.not5 = not5;
+    }
+
+    public String getNot6() {
+        return not6;
+    }
+
+    public void setNot6(String not6) {
+        this.not6 = not6;
+    }
+
+    public String getNot7() {
+        return not7;
+    }
+
+    public void setNot7(String not7) {
+        this.not7 = not7;
+    }
+    
+}
